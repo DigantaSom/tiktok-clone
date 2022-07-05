1f74d7c2e02a21becab5e298690e91902fd3e0a1
Diganta Som <itsdsom111@gmail.com>
2022-07-06 02:28:22 +0530

    Building the VideoCard component

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,4 +1,11 @@
+import { useState, useEffect, useRef } from 'react';
 import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsFillPlayFill, BsFillPauseFill, BsPlay } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
 
 import { Video } from '../types';
 
@@ -7,7 +14,94 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
-  return <div>VideoCard</div>;
+  const [isHover, setIsHover] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoPress = () => {
+    if (isPlaying) {
+      videoRef.current?.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef.current?.play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+      <div>
+        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+          <div className='w-10 h-10 md:w-16 md:h-16'>
+            <Link href='/'>
+              <Image
+                width={62}
+                height={62}
+                className='rounded-full'
+                src={post.postedBy.image}
+                alt='Profile Photo'
+                layout='responsive'
+              />
+            </Link>
+          </div>
+          <div>
+            <Link href='/'>
+              <div className='flex items-center gap-2'>
+                <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                  {post.postedBy.userName}{' '}
+                  <GoVerified className='text-blue-400 text-md' />
+                </p>
+                <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                  {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className='lg:ml-2 flex gap-4 relative'>
+        <div
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className='rounded-3xl'
+        >
+          <Link href='/'>
+            <video
+              ref={videoRef}
+              loop
+              src={post.video.asset.url}
+              className='lg:w-[600px] lg:h-[530px] md:h-[400px] w-[200px] h-[300px] rounded-2xl cursor-pointer bg-gray-400  '
+            ></video>
+          </Link>
+
+          {isHover && (
+            <div className='absolute bottom-6 left-8 cursor-pointer md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3'>
+              {isPlaying ? (
+                <button onClick={onVideoPress}>
+                  <BsFillPauseFill className='text-black text-2xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button onClick={onVideoPress}>
+                  <BsFillPlayFill className='text-black text-2xl lg:text-4xl' />
+                </button>
+              )}
+              {isMuted ? (
+                <button onClick={() => setIsMuted(false)}>
+                  <HiVolumeOff className='text-black text-2xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button onClick={() => setIsMuted(true)}>
+                  <HiVolumeUp className='text-black text-2xl lg:text-4xl' />
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoCard;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['lh3.googleusercontent.com'], // got this hostname from the error after loading image
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
