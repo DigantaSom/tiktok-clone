30bfbb228ff89751975525802bf4e4547fa13731
Diganta Som <itsdsom111@gmail.com>
2022-07-11 13:56:02 +0530

    Implementing upload video functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,8 +13,6 @@ import Footer from './Footer';
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
 
-  const userProfile = false; // TODO: change to useState for auth-check
-
   const normalLink =
     'flex items-center justify-center xl:justify-start gap-3 hover:bg-primary p-3 cursor-pointer font-semibold text-[#F51997] rounded';
 
@@ -26,6 +24,7 @@ const Sidebar = () => {
       >
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
+
       {showSidebar && (
         <div className='w-20 xl:w-400 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
           <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
@@ -38,30 +37,6 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className='px-2 py-4 hidden xl:block'>
-              <p className='text-gray-400'>
-                Login to like and comment on videos
-              </p>
-              <div className='pr-4'>
-                {/* <GoogleLogin
-                  clientId='' // TODO: to be filled
-                  render={renderProps => (
-                    <button
-                      className='bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997] cursor-pointer'
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                    >
-                      Log in
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy='single_host_origin'
-                /> */}
-              </div>
-            </div>
-          )}
 
           <Discover />
           <SuggestedAccounts />
